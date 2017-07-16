6deb2d5a0014466d20654e66c3a4b327922f83c9
Thomas Pentenrieder <pentenrieder@outlook.com>
2017-07-16 23:57:49 +0200

    upd

--- NetSpeedViewer/SampleBars/HelloWorldBar.cs
+++ NetSpeedViewer/SampleBars/HelloWorldBar.cs
@@ -3,7 +3,6 @@ using System.Windows.Forms;
 using BandObjectLib;
 using System.Runtime.InteropServices;
 using System.Drawing;
-using System.Threading.Tasks;
 using System.Net.NetworkInformation;
 using System.Collections.Generic;
 
